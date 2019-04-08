cc8a8235ea9006dde061c846c3f64c1240a1df22
Millwater <nmillwater@alertlogic.com>
2019-04-08 14:47:49 +0100

    Added responsive tables

--- static/js/discover.js
+++ static/js/discover.js
@@ -6,7 +6,7 @@ $.get("/api/discover/top-rated").done(function (response) {
         $("#highestrated")[0].innerHTML = '<div class="alert alert-danger text-center">Could not load Top Rated (' + results.status_code + ') ' + results.status_message + ' </div>'
     }else{ // Otherwise, build the results table
         category = "movie";
-        html = '<table class="table table-striped"><thead><tr><th>Rank</th><th>Title</th><th>Rating</th><th>Votes</th><th></th></tr></thead><tbody>';
+        html = '<table class="table table-striped table-responsive"><thead><tr><th>Rank</th><th>Title</th><th>Rating</th><th>Votes</th><th></th></tr></thead><tbody>';
         $(results.results).each(function(index){ // Add rows to the table for each result
             title = this.title
             html += '<tr><td>' + (index+1) + '</td><td>' + this.title + '</td><td>' + this.vote_average + '</td><td>' + this.vote_count.toLocaleString() + '</td><td><button href="#" style="cursor: pointer;" type="button" class="btn btn-info btn-block" onclick="getDetail(\'movie\',' + this.id + ');">Get details</button></td>';
@@ -21,7 +21,7 @@ $.get("/api/discover/top-trending").done(function (response) {
     if (results.status_code){ // There's been an error, so tell the user
         $("#trending")[0].innerHTML = '<div class="alert alert-danger text-center">Could not load Trending (' + results.status_code + ') ' + results.status_message + ' </div>'
     }else{ // Otherwise, build the results table
-        html = '<table class="table table-striped"><thead><tr><th>Rank</th><th>Title</th><th>Rating</th><th>Votes</th><th></th></tr></thead><tbody>';
+        html = '<table class="table table-striped table-responsive"><thead><tr><th>Rank</th><th>Title</th><th>Rating</th><th>Votes</th><th></th></tr></thead><tbody>';
         $(results.results).each(function(index){ // Add rows to the table for each result
             html += '<tr><td>' + (index+1) + '</td><td>' + this.title + '</td><td>' + this.vote_average + '</td><td>' + this.vote_count.toLocaleString() + '</td><td><button href="#" style="cursor: pointer;" type="button" class="btn btn-info btn-block" onclick="getDetail(\'movie\',' + this.id + ');">Get details</button></td>';
         });
